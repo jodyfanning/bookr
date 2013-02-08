826241355ed28fb73cf81f941c95a3b1eba83e0c
Jody Fanning <jodyfanning@gmail.com>
2013-02-08 10:28:21 +0200

    Code clean up, minor fix.
    Minor formatting and fixed import

--- test/models/BookDAOImplTest.java
+++ test/models/BookDAOImplTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bson.types.ObjectId;
 import org.junit.Test;
 
 import com.google.code.morphia.DatastoreImpl;
@@ -16,7 +17,6 @@ import com.google.code.morphia.query.FieldEnd;
 import com.google.code.morphia.query.Query;
 import com.google.code.morphia.query.UpdateOperations;
 import com.google.code.morphia.query.UpdateResults;
-import com.sun.corba.se.spi.ior.ObjectId;
 
 public class BookDAOImplTest {
 
@@ -32,10 +32,12 @@ public class BookDAOImplTest {
 	public void findsAll() {
 
 		@SuppressWarnings("serial")
-		List<Book> fakeList = new ArrayList<Book>() {{
-			add(new Book("A fake book"));
-		}};
-		
+		List<Book> fakeList = new ArrayList<Book>() {
+			{
+				add(new Book("A fake book"));
+			}
+		};
+
 		setupMocks();
 
 		@SuppressWarnings("unchecked")
@@ -43,7 +45,7 @@ public class BookDAOImplTest {
 		when(ds.find(Book.class)).thenReturn(query);
 		when(query.order(any(String.class))).thenReturn(query);
 		when(query.asList()).thenReturn(fakeList);
-		
+
 		BookDAOImpl bookDAO = new BookDAOImpl(ds);
 		List<Book> list = bookDAO.findAll();
 		assertThat(list).isEqualTo(fakeList);
@@ -55,22 +57,22 @@ public class BookDAOImplTest {
 		setupMocks();
 		Book originalBook = new Book("Another book");
 		int originalVersion = originalBook.getVersion();
-		
+
 		Query<Book> query = mock(Query.class);
 		FieldEnd end = mock(FieldEnd.class);
 		when(ds.createQuery(Book.class)).thenReturn(query);
 		when(query.field(any(String.class))).thenReturn(end);
 		when(end.equal(any(ObjectId.class))).thenReturn(query);
-		
+
 		UpdateOperations<Book> ops = mock(UpdateOperations.class);
 		when(ds.createUpdateOperations(Book.class)).thenReturn(ops);
 		when(ops.set(any(String.class), any(Object.class))).thenReturn(ops);
 		when(ops.inc(any(String.class))).thenReturn(ops);
-		
+
 		UpdateResults<Book> result = mock(UpdateResults.class);
 		when(ds.update(query, ops)).thenReturn(result);
 		when(result.getUpdatedExisting()).thenReturn(true);
-		
+
 		BookDAOImpl bookDAO = new BookDAOImpl(ds);
 		Book updatedBook = bookDAO.safeUpdate(originalBook);
 		assertThat(updatedBook.getVersion()).isEqualTo(originalVersion + 1);
